3a7cae8bbdab49fb56636b3c29e280bddec96689
gloria kitili <gloria.kitili@student.moringaschool.com>
2022-12-10 17:17:48 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,17 +1,13 @@
+
 function fetchBooks() {
-  // To pass the tests, don't forget to return your fetch!
   
+  let resultToBeReturned = fetch("https://anapioficeandfire.com/api/books") 
+  .then((resp) => resp.json()) 
+  .then((data) => renderBooks(data)) 
+  .catch((error) => `ERROR => ${console.error(error.message)}`)
+  return resultToBeReturned;
 }
+
 
 function renderBooks(books) {
-  const main = document.querySelector('main');
-  books.forEach(book => {
-    const h2 = document.createElement('h2');
-    h2.innerHTML = book.name;
-    main.appendChild(h2);
-  });
-}
-
-document.addEventListener('DOMContentLoaded', function() {
-  fetchBooks();
-});
+  const main = document.querySelector('main');}
